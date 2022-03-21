4b6cf5b4c3be1bcb17b923a23939e1d89d70444a
leonholly <lemonetch@yandex.ru>
2022-03-21 13:29:00 +0300

    .

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -4,8 +4,8 @@ from rest_framework import filters, mixins, permissions, viewsets
 from rest_framework.generics import get_object_or_404
 from reviews.models import Category, Genre, Title
 from users.permissions import AdminOrReadOnly, AuthorPermission
-from .filters import TitleFilter
 
+from .filters import TitleFilter
 from .serializers import (CategorySerializer, CommentSerializer,
                           GenreSerializer, GetTitleSerializer,
                           ReviewSerializer, TitleSerializer)
